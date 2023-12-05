e3921ffa6e036fca5b716fefed7842a064b76268
Ryan Murray <RyanRMurray@Hotmail.co.uk>
2023-12-05 11:18:15 +0000

    fix day 5 part 2

--- src/solutions/day05.rs
+++ src/solutions/day05.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::utils::solver_types::{solve_linear, SolutionLinear};
 use anyhow::Result;
 use itertools::Itertools;
@@ -23,6 +25,37 @@ fn map_to_destination(num: usize, ranges: &Vec<[usize; 3]>) -> usize {
     num
 }
 
+fn map_range(input: [usize; 2], ranges: &Vec<[usize; 3]>) -> Vec<[usize; 2]> {
+    let mut new_ranges = vec![];
+    let mut start = input[0];
+    let end = input[1];
+
+    for r in ranges {
+        if start < r[1] {
+            new_ranges.push([start, r[1] - 1]);
+            start = r[1];
+        }
+
+        if start >= r[1] && start < r[1] + r[2] - 1 {
+            if end < r[1] + r[2] {
+                let depth1 = start - r[1];
+                let depth2 = end - r[1];
+                new_ranges.push([r[0] + depth1, r[0] + depth2]);
+                start = end;
+                break;
+            } else {
+                let depth = start - r[1];
+                new_ranges.push([r[0] + depth, r[0] + r[2]]);
+                start = r[1] + r[2];
+            }
+        }
+    }
+    if start != end {
+        new_ranges.push([start, end]);
+    }
+    new_ranges
+}
+
 impl SolutionLinear<Guide, usize, usize> for Day05Solution {
     fn load(input: &str) -> Result<Guide> {
         let (seeds, maps) = input.split_once("\n\n").unwrap();
@@ -44,6 +77,7 @@ impl SolutionLinear<Guide, usize, usize> for Day05Solution {
                             .try_into()
                             .unwrap()
                     })
+                    .sorted_by(|a: &[usize; 3], b: &[usize; 3]| a[1].cmp(&b[1]))
                     .collect_vec()
             })
             .collect_vec();
@@ -69,19 +103,24 @@ impl SolutionLinear<Guide, usize, usize> for Day05Solution {
     }
 
     fn part2(input: &mut Guide, _part_1_solution: usize) -> Result<usize> {
-        let mut min = usize::MAX;
+        let ranges: HashSet<[usize; 2]> = input
+            .seeds
+            .chunks_exact(2)
+            .map(|c| [c[0], c[0] + c[1] - 1])
+            .collect();
 
-        let seed_iter = input.seeds.chunks_exact(2).flat_map(|x| x[0]..x[0] + x[1]);
-
-        for seed in seed_iter {
-            let mut result = seed;
-            for m in &input.maps {
-                result = map_to_destination(result, m);
-            }
-            min = min.min(result);
-        }
-
-        Ok(min)
+        Ok(input
+            .maps
+            .iter()
+            // for each map, create the ranges mapped from the prior set of ranges
+            .fold(ranges, |rs, m| {
+                rs.into_iter().flat_map(|r| map_range(r, m)).collect()
+            })
+            // now find the minimum amongst those ranges
+            .into_iter()
+            .map(|[min, _]| min)
+            .min()
+            .unwrap())
     }
 }
 
