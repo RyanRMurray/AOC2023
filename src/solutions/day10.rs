db870c3986bbaa7a92d3ef1aff730f0c2fab1266
Ryan Murray <RyanRMurray@Hotmail.co.uk>
2023-12-12 14:11:18 +0000

    much nicer day 10

--- src/solutions/day10.rs
+++ src/solutions/day10.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    convert::identity,
-};
+use std::{collections::HashMap, convert::identity};
 
 use crate::utils::{
     grid::Grid,
@@ -13,7 +10,7 @@ use anyhow::Result;
 use itertools::Itertools;
 use lazy_static::lazy_static;
 
-// TODO
+// implementation based on non-zero winding as described here: https://old.reddit.com/r/adventofcode/comments/18eza5g/2023_day_10_animated_visualization/kcqwjon/
 pub struct Day10Solution {}
 
 pub fn day10(input: &str) -> Result<f32> {
@@ -66,127 +63,72 @@ fn adjs(maze: &Maze, pt: &Pt<2>) -> Vec<Pt<2>> {
         .collect_vec()
 }
 
-fn find_loop(maze: &Maze, start: &Pt<2>) -> Vec<(Pt<2>, usize)> {
-    // first, find starting directions (Vec<(from, here)>)
-    let starts = Pt::<2>::card_offsets()
+fn measure_loop(maze: &Maze, start: &Pt<2>) -> (HashMap<Pt<2>, isize>, isize) {
+    // notate distances of points in loop from start
+    let mut distances: HashMap<Pt<2>, isize> = HashMap::from([(*start, 1)]);
+
+    // find start
+    let mut ptr = [Pt([0, -1]), Pt([1, 0]), Pt([0, 1]), Pt([-1, 0])]
         .iter()
         .map(|offset| (*start, offset + start))
-        .filter(|(_, to)| can_enter(maze, start, to))
-        .collect_vec();
+        .find(|(_, to)| can_enter(maze, start, to))
+        .unwrap();
+    let mut d = 2;
 
-    // notate distances of points in loop from start
-    let mut distances: HashMap<Pt<2>, usize> = HashMap::from([(*start, 0)]);
-
-    for s in starts {
-        let mut ptr = s;
-        let mut d = 1;
-        while ptr.1 != *start {
-            // note distance if shorter
-            match distances.get(&ptr.1) {
-                Some(dist) => distances.insert(ptr.1, *dist.min(&d)),
-                None => distances.insert(ptr.1, d),
-            };
-            // get next step
-            ptr = (
-                ptr.1,
-                *adjs(maze, &ptr.1).iter().find(|p| **p != ptr.0).unwrap(),
-            );
-            d += 1;
-        }
+    while ptr.1 != *start {
+        // note distance if shorter
+        match distances.get(&ptr.1) {
+            Some(dist) => distances.insert(ptr.1, *dist.min(&d)),
+            None => distances.insert(ptr.1, d),
+        };
+        // get next step
+        ptr = (
+            ptr.1,
+            *adjs(maze, &ptr.1).iter().find(|p| **p != ptr.0).unwrap(),
+        );
+        d += 1;
     }
-
-    distances.iter().map(|(k, v)| (*k, *v)).collect_vec()
+    (distances, d - 1)
 }
 
-fn valid_ns(
-    to_explore: &[Pt<2>],
-    boundary: &HashSet<Pt<2>>,
-    visited: &HashSet<Pt<2>>,
-    visit: Pt<2>,
-) -> Vec<Pt<2>> {
-    Pt::<2>::card_offsets()
-        .into_iter()
-        .filter_map(|offset| {
-            let v = &(offset + visit);
-            if to_explore.contains(v) && !boundary.contains(v) && !visited.contains(v) {
-                Some(*v)
-            } else {
-                None
-            }
-        })
-        .collect_vec()
-}
-
-fn outside_loop(ps: &HashSet<Pt<2>>, max_x: isize, max_y: isize) -> bool {
-    ps.iter()
-        .any(|Pt([x, y])| *x == 0 || *y == 0 || *x == max_x || *y == max_y)
-}
-
-// make a higher-resolution of the boundary
-fn resolve_boundary(maze: &Maze, boundary: HashSet<Pt<2>>) -> HashSet<Pt<2>> {
-    boundary.iter().fold(HashSet::new(), |mut hs, p| {
-        let new_pos = *p * 2;
-        let c = maze.grid.get(p).unwrap();
-        hs.extend(PIPES.get(c).unwrap().iter().map(|offset| offset + &new_pos));
-        hs.insert(new_pos);
-        hs
-    })
-}
-
-impl SolutionLinear<Maze, usize, usize> for Day10Solution {
+impl SolutionLinear<Maze, isize, usize> for Day10Solution {
     fn load(input: &str) -> Result<Maze> {
         Ok(load_2d_grid(input, identity))
     }
 
-    fn part1(input: &mut Maze) -> Result<usize> {
-        Ok(*find_loop(input, &find_start(input))
-            .iter()
-            .map(|(_, v)| v)
-            .max()
-            .unwrap())
+    fn part1(input: &mut Maze) -> Result<isize> {
+        Ok(measure_loop(input, &find_start(input)).1 / 2)
     }
 
-    fn part2(input: &mut Maze, _part_1_solution: usize) -> Result<usize> {
-        // oh god im too tired for flood fill. OH WELL.
+    fn part2(input: &mut Maze, _part_1_solution: isize) -> Result<usize> {
+        let ([_, _], [max_x, _]) = input.bounds();
+        let (loop_steps, size) = measure_loop(input, &find_start(input));
         let mut contained = 0;
-        let b: HashSet<Pt<2>> = find_loop(input, &find_start(input))
-            .iter()
-            .map(|(k, _)| *k)
-            .collect();
-        let boundary = resolve_boundary(input, b);
+        let mut inside = 0;
 
-        let ([_, _], [max_x, max_y]) = input.bounds();
+        for pt in input.iter_linear() {
+            let below = pt + &Pt([0, 1]);
+            let steps = loop_steps
+                .get(pt)
+                .and_then(|st1| loop_steps.get(&below).map(|st2| st1 - st2 % size));
 
-        let mut to_explore = (0..max_x * 2 + 1)
-            .flat_map(|x| (0..max_y * 2 + 1).map(move |y| Pt([x, y])))
-            .filter(|p| !boundary.contains(p))
-            .collect_vec();
-
-        while !to_explore.is_empty() {
-            // flood fill
-            let mut to_visit = vec![to_explore.pop().unwrap()];
-            let mut visited = HashSet::new();
-
-            while let Some(v) = to_visit.pop() {
-                visited.insert(v);
-                to_visit.extend(valid_ns(&to_explore, &boundary, &visited, v));
-            }
-
-            // drop visited from to_expore
-            to_explore = to_explore
-                .into_iter()
-                .filter(|p| !visited.contains(p))
-                .collect_vec();
-
-            // count if within loop
-            if !outside_loop(&visited, max_x * 2, max_y * 2) {
-                contained += visited
-                    .iter()
-                    .filter(|Pt([x, y])| (x % 2 == 0) && (y % 2 == 0))
-                    .count();
+            if pt.0[0] == max_x {
+                inside = 0;
+            } else {
+                match steps {
+                    Some(v) => {
+                        if v.abs() == 1 {
+                            inside += v
+                        }
+                    }
+                    _ => {
+                        if inside != 0 && !loop_steps.contains_key(pt) {
+                            contained += 1;
+                        }
+                    }
+                }
             }
         }
-
         Ok(contained)
     }
 }
@@ -262,7 +204,7 @@ L7JLJL-JLJLJL--JLJ.L
         80,
         10
     )]
-    fn validate_day10(#[case] input: &str, #[case] expected_1: usize, #[case] expected_2: usize) {
+    fn validate_day10(#[case] input: &str, #[case] expected_1: isize, #[case] expected_2: usize) {
         let mut input = Day10Solution::load(input).unwrap();
 
         let p1 = Day10Solution::part1(&mut input).unwrap();
